2d1b5210b54ac77905be13975cefba9c2f11a165
ppolle <peter.m.polle@gmail.com>
2015-11-23 09:05:00 +0300

    first task commit

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use Validator;
+use Auth;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+
+class AuthController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Registration & Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users, as well as the
+    | authentication of existing users. By default, this controller uses
+    | a simple trait to add these behaviors. Why don't you explore it?
+    |
+    */
+
+    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+
+    public function redirectToProvider()
+    {
+        return Socialite::driver('github')->redirect();
+    }
+
+
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', 'Auth\AuthController@redirectToProvider');
+
+Route::get('users', function()
+{
+    return 'Users!';
+});
+
+Route::get('login',function()
+{
+ return "This is the official login page";
+});
+
+Route::get('auth/github',
+    ['as'=>'githublogin',
+        'uses'=> 'Auth\AuthController@redirectToProvider']);
+Route::get('auth/github/callback', 'Auth\AuthController@handleProviderCallback');
